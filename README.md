492ea8ef889f3dbe6f7e631e94e9b194f60b26b0
Konrad Tusiński <konrad.tusinski@o2.pl>
2023-03-20 22:40:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# To-Do-List
+
+## Short Discription 
+
+To-Do-list It is an application that helps you organize your day or organize your own goals. this application uses solutions that allow you to change the added options to done or remove them from the list.
+
+## CSS
+
+the app was made using flex which helped keep it flexible. In addition, a transform was used that gave the page a lively effect
+
+## Link 
+
+*[To Do List](https://streetwolf123.github.io/To-Do-List.github.io/)
+
+## Picture of website
+
+![](https://github.com/streetwolf123/To-Do-List.github.io/blob/main/img/web.png?raw=true)
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,108 @@
+{
+    let tasks = [
+        {
+            content: "jeść",
+            done: false,
+        },
+        {
+            content: "Spać",
+            done: true,
+        },
+    ];
+
+    const addNewTask = (newTask) => {
+        tasks.push({
+            content: newTask,
+        });
+        render();
+    };
+
+    const removeTask = (tasksIndex) => {
+        tasks.splice(tasksIndex, 1);
+        render();
+
+    };
+
+    const toggleTaskDone = (tasksIndex) => {
+        tasks[tasksIndex].done = !tasks[tasksIndex].done;
+        render();
+    };
+
+
+    const render = () => {
+        let taskObject = "";
+
+        for (const task of tasks) {
+            taskObject += `
+            <li class="list__task list__tasks-flex ${task.done ? "list__tasks--done" : ""} "
+            >
+                
+                <button class="js-done li__buttonDone li__button">
+                <i class="${task.done ? "fa-solid fa-check button__i" : ""}"></i>
+                </button>
+                              
+                <p class="task-js paragraph-js paragraph-flex ${task.done ? "li__paragraph-done" : ""}"> ${task.content}</p>
+                          
+                <button class="task-js js-remove li__buttonRemove li__button">
+                <i class="fa-sharp fa-solid fa-xmark"></i>
+                </button>
+               
+               
+            </li>
+            `;
+
+        };
+        
+        document.querySelector(".list-js").innerHTML = taskObject;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const doneButtons = document.querySelectorAll(".js-done");
+        doneButtons.forEach((donebutton, index) => {
+            donebutton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+
+    }
+
+
+
+
+    const onFormSubmit = () => {
+        const newTask = document.querySelector(".form__input-js").value.trim();
+
+        if (newTask === "") {
+            return;
+        };
+
+        for (const task of tasks) {
+            if (newTask === "") {
+                return;
+            };
+
+        };
+
+        addNewTask(newTask);
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".form");
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            onFormSubmit();
+        });
+
+    };
+
+    init();
+
+};
