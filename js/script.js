ae28d2e7d8c9e4109d49db6f8aaa6e8e0a464e6e
Konrad Tusiński <konrad.tusinski@o2.pl>
2023-03-21 08:51:49 +0100

    upgrade css, js and html

--- js/script.js
+++ js/script.js
@@ -28,34 +28,9 @@
         render();
     };
 
-
-    const render = () => {
-        let taskObject = "";
-
-        for (const task of tasks) {
-            taskObject += `
-            <li class="list__task list__tasks-flex ${task.done ? "list__tasks--done" : ""} "
-            >
-                
-                <button class="js-done li__buttonDone li__button">
-                <i class="${task.done ? "fa-solid fa-check button__i" : ""}"></i>
-                </button>
-                              
-                <p class="task-js paragraph-js paragraph-flex ${task.done ? "li__paragraph-done" : ""}"> ${task.content}</p>
-                          
-                <button class="task-js js-remove li__buttonRemove li__button">
-                <i class="fa-sharp fa-solid fa-xmark"></i>
-                </button>
-               
-               
-            </li>
-            `;
-
-        };
-        
-        document.querySelector(".list-js").innerHTML = taskObject;
-
+    const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
+
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
@@ -63,11 +38,44 @@
         });
 
         const doneButtons = document.querySelectorAll(".js-done");
+
         doneButtons.forEach((donebutton, index) => {
             donebutton.addEventListener("click", () => {
                 toggleTaskDone(index);
             });
         });
+
+
+    }
+
+
+    const render = () => {
+        let taskObject = "";
+
+        for (const task of tasks) {
+            taskObject += `
+            <li class="list__task list__tasks-flex ${task.done ? "list__tasks--done" : ""} ">
+                
+                <button class="js-done li__buttonDone li__button">
+                    <i class="${task.done ? "fa-solid fa-check button__i" : ""}"></i>
+                </button>
+                              
+                <p class="task-js paragraph-js paragraph-flex ${task.done ? "li__paragraph-done" : ""}"> ${task.content}</p>
+                          
+                <button class="task-js js-remove li__buttonRemove li__button">
+                    <i class="fa-sharp fa-solid fa-xmark"></i>
+                </button>
+               
+               
+            </li>
+            `;
+        
+        };
+        
+        document.querySelector(".list-js").innerHTML = taskObject;
+        bindEvents();
+
+       
 
     }
 
@@ -75,32 +83,33 @@
 
 
     const onFormSubmit = () => {
-        const newTask = document.querySelector(".form__input-js").value.trim();
+
+        const newTask = document.querySelector(".form__input-js");
 
         if (newTask === "") {
+
             return;
-        };
 
-        for (const task of tasks) {
-            if (newTask === "") {
-                return;
-            };
+        } else {
 
-        };
-
-        addNewTask(newTask);
+            addNewTask(newTask.value.trim());
+        };  
     };
 
     const init = () => {
         render();
 
         const form = document.querySelector(".form");
+
         form.addEventListener("submit", (event) => {
             event.preventDefault();
+            
 
             onFormSubmit();
-        });
 
+            
+
+        });
     };
 
     init();
